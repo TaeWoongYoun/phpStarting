e0d3f84ffc94a1cfc6c22183430a75d36b5bb381
xodnd0507 <24457545yong@gmail.com>
2024-05-10 09:36:51 +0900

    test : Variable php

---
+++ variable/variable.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variable</title>
+</head>
+<body>
+    <?php
+        // $a = 1;
+        // echo $a + 1;
+
+        $name = "hello";
+
+        echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Accusantium asperiores ".$name." esse earum ipsa optio totam! Eos, 
+        corrupti facere consequatur laborum dolore, 
+        ipsum cupiditate id corporis minima eligendi nesciunt voluptatum obcaecati.";
+    ?>
+</body>
+</html>
