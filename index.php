61903fda6bee3ddba039cf1dc600a3c1be8cccb4
xodnd0507 <24457545yong@gmail.com>
2024-05-10 09:14:29 +0900

    test : Test php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    <?php
+        echo date('Y-m-d H:i:s')
+    ?>
+</body>
+</html>
