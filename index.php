eb4a4fd337b4805d7d398600e2edd0b2fe57dc68
xodnd0507 <24457545yong@gmail.com>
2024-05-11 17:44:35 +0900

    test : Test for and data

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
 </head>
 <body>
     <h1><a href="index.php">WEB</a></h1>
+    <?php
+        $list = scandir('data');
+        $i = 0;
+        while($i < count($list)){
+            if($list[$i] != '.'){
+                if($list[$i] != '..'){
+                    ?>
+                <li><a href="index.php?id=<?=$list[$i]?>"><?=$list[$i]?></a></li>
+                <?php
+                }
+            }
+            $i = $i + 1;
+        }
+    ?>
     <ol>
         <li><a href="index.php?id=HTML">HTML</a></li>
         <li><a href="index.php?id=CSS">CSS</a></li>
