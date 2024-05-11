1da31df62944a7fa4b861a85f2615f49b9fc4680
xodnd0507 <24457545yong@gmail.com>
2024-05-11 18:47:18 +0900

    test : Test array

---
+++ array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>array</title>
+</head>
+<body>
+    <?php
+    $object = array(
+        'name1' => '태웅',
+        'name2' => '도연',
+        'name3' => '정빈',
+        'name4' => '예준',
+        'name5' => '필립'
+    );
+
+    $array = array('성우', '민석', '태연', '윤상', '성호');
+    echo $array[3]. '<br>';
+    echo $object['name1'];
+    ?>
+</body>
+</html>
