9087f7f2494075d62e318b3584bd4fb5ca783759
xodnd0507 <24457545yong@gmail.com>
2024-05-11 15:05:09 +0900

    test : Test conditional

---
+++ conditional.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>conditional</title>
+</head>
+<body>
+    <h2>if</h2>
+    <?php
+        echo '1<br>';
+        if (false) {
+            echo '2<br>';
+        } else {
+            echo '2-2<br>';
+        }
+        echo '3<br>';
+    ?>
+</body>
+</html>
