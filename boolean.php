3c6ebd01ade87a29bdb12e13ff1ddf460751eaef
xodnd0507 <24457545yong@gmail.com>
2024-05-11 14:42:31 +0900

    test : Test boolean and var_dump

---
+++ boolean.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>비교연산자</title>
+</head>
+<body>
+    <?php
+        var_dump(1==2)
+    ?>
+</body>
+</html>
