fcf9ce19d84a0d87b5b1637c7e49ded51ad3a32b
xodnd0507 <24457545yong@gmail.com>
2024-05-13 13:20:36 +0900

    chore : Change code

--- index.php
+++ index.php
@@ -21,11 +21,6 @@
             $i = $i + 1;
         }
     ?>
-    <!-- <ol>
-        <li><a href="index.php?id=HTML">HTML</a></li>
-        <li><a href="index.php?id=CSS">CSS</a></li>
-        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
-    </ol> -->
     <h2>
         <?php 
             if (isset($_GET['id'])) {
