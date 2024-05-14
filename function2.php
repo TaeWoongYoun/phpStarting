0c18cc6036fdf7b65cd0f03ef27dd42d6ef0df05
xodnd0507 <24457545yong@gmail.com>
2024-05-14 16:19:37 +0900

    test : Function

---
+++ function2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Function</h1>
+    <h2>Basic</h2>
+
+    <?php
+        function basic(){
+            print("Lorem ipsum dolor sit1<br>");
+            print("Lorem ipsum dolor sit2");
+        }
+
+        basic();
+    ?>
+</body>
+</html>
