dc0babed43c1d81e0db6fb937c24745ed388ed7c
xodnd0507 <24457545yong@gmail.com>
2024-05-15 21:43:06 +0900

    test : Test form method post

--- form.php
+++ form.php
@@ -7,6 +7,8 @@
 </head>
 <body>
 <?php
+    file_put_contents('data/'.$_POST['title'], $_POST['discription']);
+
     echo "<p>title : " .$_POST['title'].  "</p>";
     echo "<p>discription : " .$_POST['discription'].  "</p>";
 ?>
