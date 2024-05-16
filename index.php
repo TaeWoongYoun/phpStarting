3cdc19867a0d8253711fdf501154fd6b7064c7d0
xodnd0507 <24457545yong@gmail.com>
2024-05-16 10:24:59 +0900

    test : Update test

--- index.php
+++ index.php
@@ -49,6 +49,9 @@
     ?>
     </ol>
     <a href="create.php">create</a>
+    <?php if(isset($_GET['id'])){ ?>
+        <a href="update.php?id=<?=$_GET['id']?>">update</a>
+    <?php } ?>
     <h2>
         <?php 
             title();
